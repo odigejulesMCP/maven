3be314a3fe955dbfb5b6c1362e40d05e1352a526
ojules30@outlook.com <jenkins@localhost.localdomain>
2023-06-05 23:56:33 -0400

    Test webhook2

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World, the Miami Heat is going to win the NBA finals!!!!", app.getMessage());
+        assertEquals("Hello World, the Miami Heat is going to win the NBA finals!!!!!", app.getMessage());
     }
 }
